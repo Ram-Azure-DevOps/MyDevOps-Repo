9d629b8d1a39f94735945c7cf44bf46ced31972c
Ram3091 <ramrajak3091@gmail.com>
2026-08-10 01:25:29 +0530

    ignore commit 1

--- module/azurerm_recource_group/versions.tf
+++ module/azurerm_recource_group/versions.tf
@@ -1,13 +1,10 @@
 terraform {
-   required_version = ">= 1.6.0"
+  required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 5.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
